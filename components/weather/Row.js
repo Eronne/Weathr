16a59bd0fd45a78039538375f85d3d626d87fcbc
Eronne <erwann.letue@gmail.com>
2017-06-25 20:32:59 -0400

    Added icons on list

--- components/weather/Row.js
+++ components/weather/Row.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {
     View,
-    Text
+    Text,
+    Image
 } from "react-native";
 import style from '../../styles/Styles'
 import row from '../../styles/Row'
@@ -15,25 +16,45 @@ export default class Row extends React.Component {
         index: React.PropTypes.number
     };
 
+    setIcon(size = 50) {
+        const type = this.props.day.weather[0].main.toLowerCase();
+        let img;
+        switch (type) {
+            case 'clouds':
+                img = require('../icons/weather/clouds.png');
+                break;
+            case 'rain':
+                img = require('../icons/weather/rain.png');
+                break;
+            default:
+                img = require('../icons/weather/sun.png');
+                break;
+        }
+        return <Image source={img} style={{width: size, height: size}} />
+    }
+
     day() {
         let day = moment(this.props.day.dt * 1000).format('dddd');
         return (
-            <Text>{day.toUpperCase()}</Text>
+            <Text style={[row.white, row.bold]}>{day.toUpperCase()}</Text>
         )
     }
 
     date() {
         let date = moment(this.props.day.dt * 1000).format('DD/MM');
         return (
-            <Text>{date}</Text>
+            <Text style={row.white}>{date}</Text>
         )
     }
 
     render() {
         return (
-            <View>
-                <Text>{this.day()} {this.date()}</Text>
-                <Text>{this.props.day.temp.day}°C</Text>
+            <View style={row.view}>
+                {this.setIcon()}
+                <View>
+                    <Text>{this.day()} {this.date()}</Text>
+                    <Text style={[row.white, row.bold, row.temp]}>{this.props.day.temp.day}°C</Text>
+                </View>
             </View>
         )
     }
---
+++ styles/Row.js
@@ -0,0 +1,28 @@
+const blue = '#3498db';
+const white = '#ecf0f1';
+
+export default ({
+    white: {
+        color: '#FFF'
+    },
+
+    bold: {
+        fontWeight: 'bold'
+    },
+
+    view: {
+        backgroundColor: blue,
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderBottomColor: white,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    temp: {
+        fontSize: 22
+    }
+})
